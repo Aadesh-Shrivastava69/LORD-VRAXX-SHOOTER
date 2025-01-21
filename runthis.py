4411d6433de487f404a6f4d3573f4ddb1f415332
Aadesh Shrivastava <137671138+Aadesh-Shrivastava69@users.noreply.github.com>
2025-01-22 00:40:19 +0530

    update

--- runthis.py
+++ runthis.py
@@ -1,13 +1,72 @@
-import pygame 
+import pygame , sys
 import math
 import random
 from pygame import mixer
 
 pygame.init()
 
+game_state = 'menu'
+
+
+
+    
+
+
+
 #creating screen 
 screen = pygame.display.set_mode((800 , 600))
 pygame.display.set_caption("lord vraxx shooter")
+
+
+
+#load buttons
+
+start_img = pygame.image.load("start_img.png").convert_alpha()
+play_again = pygame.image.load("play_again.png").convert_alpha()
+#quit = pygame.image.load("")
+
+
+# button class
+
+class Button():
+    def __init__(self,x,y,image, scale):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image,(int(width*scale) , int(height*scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+        self.clicked = False
+
+    def draw(self):
+
+        action = False
+        global game_state
+        
+        #position
+        pos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                print('clicked')
+                action = True
+           
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+
+
+        screen.blit(self.image,(self.rect.x,self.rect.y))
+        return action
+        
+         
+
+#button instances 
+start_button = Button(205,120,start_img , 0.1)
+retry_button = Button(205,200,play_again , 0.1)
+
+
 
 #background
 
@@ -66,7 +125,7 @@ textY = 10
 
 #game over
 
-over_font = pygame.font.Font('freesansbold.ttf',64)
+over_font = pygame.font.Font('freesansbold.ttf',56)
 
 
 #defining
@@ -76,8 +135,14 @@ def show_score(x,y):
     screen.blit(score , (x,y))
 
 def game_over():
+    global game_state
     over_text  = over_font.render('VRAXXED !!!!!!!',True,(255,255,255))
     screen.blit(over_text,(200,250))
+    if retry_button.draw():
+        game_state = 'play'
+        print("oops")
+        
+
     
 
 
@@ -101,99 +166,151 @@ def iscollision(enemyX,enemyY,bulletX,bulletY):
 
 
 
+def menu():
+    global playerX
+    global playerY
+    global playerX_change
+    global playerY_change
+    global game_state
+    run = True
+    while run:
+        if game_state == 'menu':
+            screen.fill((0,0,0))
+
+            if start_button.draw():
+                game_state = 'play'
+                print("play")
+                play()
+
+            #font1 = pygame.font.Font('freesansbold.ttf', 64)
+            #text1 = font1.render("Press Enter to play",True,(255,255,255))
+            #screen.blit(text1,(112,200))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                    game_state = 'play'
+                    play()
+                
+
+                    
+            pygame.display.update()
+
 #game loop
 
-
-running = True 
-while running:
+def play():
+    global playerX
+    global playerY
+    global playerX_change
+    global playerY_change
+    global bulletX
+    global bulletY
+    global bulletX_change
+    global bulletY_change
+    global bullet_state
+    global score_value
+    global game_state
+    
+    running = True 
+    while running:
+        
+        if game_state == 'play':
 
     
 
-    # rgb background
+        # rgb background
 
-    screen.fill((2,0,0))
-    screen.blit(bg,(0,0))
+            screen.fill((2,0,0))
+            screen.blit(bg,(0,0))
 
 
-    for event in pygame.event.get():
+            for event in pygame.event.get():
 
-        if event.type == pygame.QUIT:
-            running = False
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    sys.exit()
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-               playerX_change = -0.4   
-            if event.key == pygame.K_RIGHT:
-                playerX_change = 0.4
-            if event.key == pygame.K_SPACE:
-                if bullet_state is 'ready':
-                    bulletX = playerX
-                    firebullet(bulletX,bulletY)   
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                playerX_change = 0  
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        playerX_change = -0.4   
+                    if event.key == pygame.K_RIGHT:
+                        playerX_change = 0.4
+                    if event.key == pygame.K_SPACE:
+                        if bullet_state is 'ready':
+                            bulletX = playerX
+                            firebullet(bulletX,bulletY)   
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                        playerX_change = 0  
                      
-# player boundary
+    # player boundary
 
-    playerX += playerX_change
-    if playerX <= 0:
-        playerX = 0
+            playerX += playerX_change
+            if playerX <= 0:
+                playerX = 0
 
-    elif playerX >= 736:
-        playerX = 736
+            elif playerX >= 736:
+                playerX = 736
 
-#enemy movement
+    #enemy movement
 
-    for i in range(num_enimies):
+            for i in range(num_enimies):
 
-        #GAME OVER
-        if enemyY[i] >= 440:
-            for j in range(num_enimies):
-                enemyY[j] = 2000
-            game_over()
-            end = mixer.Sound('LORD.wav')
-            end.play()
-            break
-             
+                #GAME OVER
+                if enemyY[i] >= 440:
+                    for j in range(num_enimies):
+                        
+                        enemyY[j] = 2000
+                        playerX = 1000
+                        playerY = 1000
+                    game_over()
+                        
+                        
+                    end = mixer.Sound('LORD.wav')
+                    end.play()
+                    break
+                
 
-        enemyX[i] += enemyX_change[i]
-        if enemyX[i] <= 0:
-            enemyX_change[i] = 0.3
-            enemyY[i] += enemyY_change[i]
+                enemyX[i] += enemyX_change[i]
+                if enemyX[i] <= 0:
+                    enemyX_change[i] = 0.3
+                    enemyY[i] += enemyY_change[i]
 
-        elif enemyX[i] >= 700:
-            enemyX_change[i] = -0.3
-            enemyY[i] += enemyY_change[i]
+                elif enemyX[i] >= 700:
+                    enemyX_change[i] = -0.3
+                    enemyY[i] += enemyY_change[i]
 #collision
-        collision = iscollision(enemyX[i],enemyY[i],bulletX,bulletY)
-        if collision:
+                collision = iscollision(enemyX[i],enemyY[i],bulletX,bulletY)
+                if collision:
 
-            exlpodesound= mixer.Sound('collision.wav')
-            exlpodesound.play()
-            bulletY = 480
-            bullet_state = 'ready'
-            score_value += 1
-            enemyX[i]= random.randint(0,736)
-            enemyY[i] = random.randint(0,150)
-        enemy(enemyX[i],enemyY[i],i)    
+                    exlpodesound= mixer.Sound('collision.wav')
+                    exlpodesound.play()
+                    bulletY = 480
+                    bullet_state = 'ready'
+                    score_value += 1
+                    enemyX[i]= random.randint(0,736)
+                    enemyY[i] = random.randint(0,150)
+                enemy(enemyX[i],enemyY[i],i)    
             
 
 #bullet movement
-    if bullet_state is 'fire':
-        firebullet(bulletX,bulletY)
-        bulletY -= bulletY_change
-    if bulletY <= 0:
-        bulletY = 480
-        bullet_state = 'ready'
+            if bullet_state is 'fire':
+                firebullet(bulletX,bulletY)
+                bulletY -= bulletY_change
+            if bulletY <= 0:
+                bulletY = 480
+                bullet_state = 'ready'
 
 
 
-    player(playerX,playerY)
-    show_score(textX,textY)
-    
-    pygame.display.update()
+            player(playerX,playerY)
+            show_score(textX,textY)
+            
+            pygame.display.update()
 
 
+menu()
 
     
 
